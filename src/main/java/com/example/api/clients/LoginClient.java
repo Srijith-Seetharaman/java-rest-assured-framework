a1a6b1cd3446be7c8ea252c390cb4096fee5211b
Srijith-Seetharaman <78795366+srijith-seetharaman@users.noreply.github.com>
2025-12-26 18:29:56 +0530

    Fixing some errors caused by a merge conflict
    Signed-off-by: Srijith-Seetharaman <78795366-Srijith-Seetharaman@users.noreply.github.com>

--- src/main/java/com/example/api/clients/LoginClient.java
+++ src/main/java/com/example/api/clients/LoginClient.java
@@ -3,12 +3,17 @@ package com.example.api.clients;
 import com.example.api.endpoints.Endpoints;
 import com.example.api.models.request.UserLoginRequestPayload;
 import com.example.api.models.response.UserLoginResponsePayload;
+import com.example.api.utils.SerialiseUtils;
+import io.restassured.response.Response;
 
 public class LoginClient {
     public static UserLoginResponsePayload login(UserLoginRequestPayload userLoginCredentials) {
         RestClient restClient = new RestClient();
 
+        Response response = restClient.post(
                 Endpoints.LOGIN.getPath(),
+                userLoginCredentials
         );
+        return SerialiseUtils.deserialize(response, UserLoginResponsePayload.class);
     }
 }
